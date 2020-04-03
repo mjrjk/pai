e6754b7d0e5c55725056d2ae5f59455315dcc963
Maja <57007921+mjrjk@users.noreply.github.com>
2020-04-03 13:26:56 +0200

    Task 2 done

--- javascript/form_check.js
+++ javascript/form_check.js
@@ -2,8 +2,8 @@ function isWhiteSpaceOrEmpty(str) {
   return /^[\s\t\r\n]*$/.test(str);
 }
 
-function checkString(str, msg){
-  if ((isEmpty(str) == true)||(isWhiteSpaceOrEmpty(str) == true)){
+function checkString(str, msg) {
+  if ((isEmpty(str) == true) || (isWhiteSpaceOrEmpty(str) == true)) {
     alert(msg);
     return false;
   } else {
@@ -12,42 +12,39 @@ function checkString(str, msg){
 }
 
 function isEmailInvalid(str) {
- let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
- if (email.test(str))
- return true;
- else {
- return false;
- }
-}
-
-function checkStringAndFocus(obj, msg, type) {
-   let str = obj.value;
-   let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
-   if (type == "email"){
-     if ((isWhiteSpaceOrEmpty(str))||(!checkEmail(str))) {
-       document.getElementById(errorFieldName).innerHTML = msg;
-       obj.focus();
-       return false;
-     }
-     else {
-       document.getElementById(errorFieldName).innerHTML = "";
-       return true;
-     }
-   }
-   else {
-     if (isWhiteSpaceOrEmpty(str)) {
-       document.getElementById(errorFieldName).innerHTML = msg;
-       obj.focus();
-       return false;
-     }
-     else {
-       document.getElementById(errorFieldName).innerHTML = "";
-       return true;
-     }
+  let email = /^[a-zA-Z_0-9\.]+@[a-zA-Z_0-9\.]+\.[a-zA-Z][a-zA-Z]+$/;
+  if (email.test(str))
+    return true;
+  else {
+    return false;
   }
 }
 
-function validate(formularz){
+function checkStringAndFocus(obj, msg, type) {
+  let str = obj.value;
+  let errorFieldName = "e_" + obj.name.substr(2, obj.name.length);
+  if (type == "email") {
+    if ((isWhiteSpaceOrEmpty(str)) || (!checkEmail(str))) {
+      document.getElementById(errorFieldName).innerHTML = msg;
+      obj.focus();
+      return false;
+    } else {
+      document.getElementById(errorFieldName).innerHTML = "";
+      return true;
+    }
+  } else {
+    if (isWhiteSpaceOrEmpty(str)) {
+      document.getElementById(errorFieldName).innerHTML = msg;
+      obj.focus();
+      return false;
+    } else {
+      document.getElementById(errorFieldName).innerHTML = "";
+      return true;
+    }
+  }
+}
+
+function validate(formularz) {
   if (checkStringAndFocus(formularz.elements["f_imie"], "Podaj imię!", "") == false) {
     return false;
   } else if (checkStringAndFocus(formularz.elements["f_nazwisko"], "Podaj nazwisko!", "") == false) {
@@ -66,49 +63,52 @@ function validate(formularz){
 }
 
 function showElement(e) {
- document.getElementById(e).style.visibility = 'visible';
+  document.getElementById(e).style.visibility = 'visible';
 }
+
 function hideElement(e) {
- document.getElementById(e).style.visibility = 'hidden';
+  document.getElementById(e).style.visibility = 'hidden';
 }
 
 function alterRows(i, e) {
- if (e) {
- if (i % 2 == 1) {
- e.setAttribute("style", "background-color: Aqua;");
- }
- e = e.nextSibling;
- while (e && e.nodeType != 1) {
- e = e.nextSibling;
- }
- alterRows(++i, e);
- }
+  if (e) {
+    if (i % 2 == 1) {
+      e.setAttribute("style", "background-color: Aqua;");
+    }
+    e = e.nextSibling;
+    while (e && e.nodeType != 1) {
+      e = e.nextSibling;
+    }
+    alterRows(++i, e);
+  }
 }
 
 function nextNode(e) {
- while (e && e.nodeType != 1) {
- e = e.nextSibling;
- }
- return e;
+  while (e && e.nodeType != 1) {
+    e = e.nextSibling;
+  }
+  return e;
 }
+
 function prevNode(e) {
- while (e && e.nodeType != 1) {
- e = e.previousSibling;
- }
- return e;
+  while (e && e.nodeType != 1) {
+    e = e.previousSibling;
+  }
+  return e;
 }
+
 function swapRows(b) {
- let tab = prevNode(b.previousSibling);
- let tBody = nextNode(tab.firstChild);
- let lastNode = prevNode(tBody.lastChild);
- tBody.removeChild(lastNode);
- let firstNode = nextNode(tBody.firstChild);
- tBody.insertBefore(lastNode, firstNode);
+  let tab = prevNode(b.previousSibling);
+  let tBody = nextNode(tab.firstChild);
+  let lastNode = prevNode(tBody.lastChild);
+  tBody.removeChild(lastNode);
+  let firstNode = nextNode(tBody.firstChild);
+  tBody.insertBefore(lastNode, firstNode);
 }
 
 function cnt(form, msg, maxSize) {
- if (form.value.length > maxSize)
-  form.value = form.value.substring(0, maxSize);
- else
-  msg.innerHTML = maxSize - form.value.length;
+  if (form.value.length > maxSize)
+    form.value = form.value.substring(0, maxSize);
+  else
+    msg.innerHTML = maxSize - form.value.length;
 }
